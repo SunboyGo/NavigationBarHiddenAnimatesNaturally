904b0669c7a7c1d8be0999f332ad2a2d69c6c3b8
Naoki Morita <namorit@gmail.com>
2015-09-16 01:50:50 +0900

    Initial commit

---
+++ NavigationBarHiddenAnimatesNaturally/PageViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PageViewController.swift
+//  NavigationBarNatuallyHideAnimation
+//
+//  Created by MORITANAOKI on 2015/09/15.
+//  Copyright (c) 2015年 molabo. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, NavigationBarHidable {
+
+    var navigationBarHidden = false
+    
+    static func instantiateFromNib() -> PageViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("PageViewController") as! PageViewController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        delegate = self
+        
+        if let nc = navigationController {
+            for r in view.gestureRecognizers as! [UIGestureRecognizer] {
+                r.requireGestureRecognizerToFail(nc.interactivePopGestureRecognizer)
+            }
+        }
+        
+        let vc = ViewController1.instantiateFromNib()
+        vc.view.backgroundColor = UIColor.yellowColor()
+        setViewControllers([vc], direction: .Forward, animated: false) { _ in }
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let vc = ViewController1.instantiateFromNib()
+        vc.view.backgroundColor = UIColor.yellowColor()
+        return vc
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let vc = ViewController1.instantiateFromNib()
+        vc.view.backgroundColor = UIColor.grayColor()
+        return vc
+    }
+}
